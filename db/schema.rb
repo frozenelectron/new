ea146731784c22b4addf6160c60d3fe9773676bf
Bhavesh <chessbhavesh@gmail.com>
2023-04-21 12:26:08 +0530

    Before adding the sign-up account feature.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_131524) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_055742) do
   create_table "users", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
