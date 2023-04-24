74643d8c8a5b7e097a42c823e0762657fe6e2713
Bhavesh <chessbhavesh@gmail.com>
2023-04-24 22:25:40 +0530

    A comment about what the get , to: , as: : signifies in the routes.rb file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "about_us", to: "about#index", as: :about
+  get "about_us", to: "about#index", as: :about # as: makes the prefix as you want.
   get "", to: "main#index"
   root "main#index"
   # Defines the root path route ("/")
